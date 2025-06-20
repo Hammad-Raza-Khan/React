71248bac8f9c412911212821f2efe6752d37c9ba
Hammad Raza Khan <hammadrazakhann@gmail.com>
2025-06-20 04:29:30 +0000

    Modifications of main files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; //core libraries for references
+import ReactDOM from 'react-dom/client'; //implementation for web like react-native is an implementation for mobile app
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
